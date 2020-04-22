884225dbb43f4b9405341b9d0d8ebc57cd475c0b
akansha-singh <singhakansha281@gmail.com>
2020-04-22 15:51:35 +0530

    Personal-details component added

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,7 @@
 //parent component
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
 
 export class UserForm extends Component {
     state = {
@@ -49,7 +50,14 @@ export class UserForm extends Component {
                     />
                 )
                 case 2:
-                    return <h1>FormPersonalDetails</h1>
+                    return (
+                        <FormPersonalDetails 
+                            nextStep={this.nextStep}
+                            prevStep={this.prevStep}
+                            handleChange={this.handleChange}
+                            values={values}
+                        />
+                    )
                 case 3:
                     return <h1>Confirm</h1>
                 case 4:
